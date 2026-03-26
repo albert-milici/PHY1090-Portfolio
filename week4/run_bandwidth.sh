d4900b23baf387c913d8fb6f027d7190100b7a48
Albert <albertmilicip23@gmail.com>
2026-03-26 18:15:40 +0000

    adjusted bash list for testing

--- week4/run_bandwidth.sh
+++ week4/run_bandwidth.sh
@@ -6,7 +6,7 @@ NUM_PINGS=1000
 
 echo "size_bytes,counter,elapsed_s,average_s" > "$OUTPUT"
 
-for size in 8 36 64 288 512 2304 4096 18432 32768 147456 262144 635760 1048576 1572864 2097152; do
+for size in 8 36 64 288 512 2304 4096 18432 32768 147456 262144 635760 1048576; do
 
     result=$(mpirun -n 2 ./bin/pingpong_bandwidth $NUM_PINGS $size)
     counter=$(echo "$result" | grep -oP 'Counter: \K[0-9]+')
