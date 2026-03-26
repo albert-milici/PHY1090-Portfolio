e175dbb2d74cebec38671481c7e83eebfcdb1fdc
Albert <albertmilicip23@gmail.com>
2026-03-26 15:57:15 +0000

    added timing helper functions

--- week4/comm_test_mpi.c
+++ week4/comm_test_mpi.c
@@ -8,6 +8,9 @@ void client_task(int my_rank, int uni_size);
 void check_uni_size(int my_rank, int uni_size);
 void check_task(int my_rank, int uni_size);
 
+double to_second_float(struct timespec in_time);
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time);
+
 int main(int argc, char **argv) 
 {
 	// declare and initialise error handling variable
@@ -110,4 +113,48 @@ void check_task(int my_rank, int uni_size)
 	{
 		client_task(my_rank, uni_size);
 	} // end else // i.e. (0 != my_rank)
+}
+
+double to_second_float(struct timespec in_time)
+{
+	// creates and initialises the variables
+	float out_time = 0.0;
+	long int seconds, nanoseconds;
+	seconds = nanoseconds = 0;
+
+	// extracts the elements from in_time
+	seconds = in_time.tv_sec;
+	nanoseconds = in_time.tv_nsec;
+
+	// calculates the time in seconds by adding the seconds and the nanoseconds divided by 1e9
+	out_time = seconds + nanoseconds/1e9;
+
+	// returns the time as a double
+	return out_time;
+}
+
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time)
+{
+	// creates and initialises the variables
+	struct timespec time_diff;
+	long int seconds, nanoseconds;                                                                                                       seconds = nanoseconds = 0;
+	double runtime = 0.0;
+
+	// extracts the elements from start_time and end_time
+	seconds = end_time.tv_sec - start_time.tv_sec;
+	nanoseconds = end_time.tv_nsec - start_time.tv_nsec;
+
+	// if the ns part is negative
+	if (nanoseconds < 0)
+	{
+		// "carry the one!"
+		seconds = seconds - 1;
+		nanoseconds = ((long int) 1e9) - nanoseconds;
+	}
+
+	// creates the runtime
+	time_diff.tv_sec = seconds;
+	time_diff.tv_nsec = nanoseconds;
+
+	return time_diff;
 }
