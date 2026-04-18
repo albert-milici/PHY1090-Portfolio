40630c7e22bfacd6baec768957f0338aaea959fc
Albert <albertmilicip23@gmail.com>
2026-04-18 20:07:38 +0100

    converted to Pytorch with GPU support

--- week9/src/grover_gpu.py
+++ week9/src/grover_gpu.py
@@ -1,80 +1,81 @@
 import numpy as np
+import torch as pt
+pt.autograd.set_grad_enabled(False)      # disable autogradients
+if pt.cuda.is_available():               # check if GPU is available
+    print("GPU available") 
+else:
+    print("Sorry, only CPU available")
+import time
 
-workspace = np.array([[1.]])
+# print(pt.cuda.is_available())       
+# print(pt.version.cuda)              
+# print(pt.cuda.get_device_name(0))   
+
 namestack = []
 
 def pushQubit(name,weights):
     global workspace
     global namestack
-    if workspace.shape == (1,1):
-        namestack = []
+    if (workspace.shape[0],workspace.shape[1]) == (1,1): #!
+        namestack = []                                   # reset if workspace empty 
     namestack.append(name)
-    weights = weights/np.linalg.norm(weights)
-    weights = np.array(weights,dtype=workspace[0,0].dtype)
-    workspace = np.reshape(workspace,(1,-1))
-    workspace = np.kron(workspace,weights)
-
+    weights = weights/np.linalg.norm(weights)            # normalize 
+    weights = pt.tensor(weights,device=workspace.device, #!
+                        dtype=workspace[0,0].dtype)      #! 
+    workspace = pt.reshape(workspace,(1,-1))             #! 
+    workspace = pt.kron(workspace,weights)               #!
+    
 def tosQubit(name):
     global workspace
     global namestack
-    k = len(namestack)-namestack.index(name)
-    if k > 1:
+    k = len(namestack)-namestack.index(name)                  # position of qubit 
+    if k > 1:                                                 # if non-trivial
         namestack.append(namestack.pop(-k))
-        workspace = np.reshape(workspace,(-1,2,2**(k-1)))
-        workspace = np.swapaxes(workspace,-2,-1)
-
+        workspace = pt.reshape(workspace,(-1,2,2**(k-1)))     #! 
+        workspace = pt.swapaxes(workspace,-2,-1)              #!
+        
 def applyGate(gate,*names):
     global workspace
-    if list(names) != namestack[-len(names):]:
-        for name in names:
+    if list(names) != namestack[-len(names):]:                # reorder stack
+        for name in names:                                    # if necessary 
             tosQubit(name)
-    workspace = np.reshape(workspace,(-1,2**(len(names))))
+    workspace = pt.reshape(workspace,(-1,2**len(names)))      #!
     subworkspace = workspace[:,-gate.shape[0]:]
-    np.matmul(subworkspace,gate.T,out=subworkspace)
-
-def probQubit(name):
-    global workspace
-    tosQubit(name)
-    workspace = np.reshape(workspace,(-1,2))
-    prob = np.linalg.norm(workspace,axis=0)**2
-    return prob/prob.sum()
-
-def measureQubit(name):
+    gate = pt.tensor(gate.T,device=workspace.device,          #! 
+                     dtype=workspace[0,0].dtype)              #! 
+    if workspace.device.type == 'cuda':                       #! 
+        pt.matmul(subworkspace,gate,out=subworkspace)         #!
+    else:    #! workaround for issue #114350 in torch.matmul 
+        subworkspace[:,:]=pt.matmul(subworkspace,gate) #!
+        
+def probQubit(name):                             # Check probabilities
+    global workspace                             # of qubit being 0 or 1
+    tosQubit(name)                               # qubit to TOS
+    workspace = pt.reshape(workspace,(-1,2))     #! to 2 cols
+    prob = pt.linalg.norm(workspace,axis=0)**2   #! compute prob 
+    prob = pt.Tensor.cpu(prob).numpy()           #! convert to numpy
+    return prob/prob.sum()                       # make sure sum is one
+    
+def measureQubit(name):                          # Measure and pop qubit
     global workspace
     global namestack
-    prob = probQubit(name)
-    measurement = np.random.choice(2,p=prob)
-    workspace = (workspace[:,[measurement]]/
-                 np.sqrt(prob[measurement]))
-    namestack.pop()
-    return str(measurement)
+    prob = probQubit(name)                      # Compute probabilities
+    measurement = np.random.choice(2,p=prob)    # 0 or 1 
+    workspace = (workspace[:,[measurement]]/    # extract col
+                 np.sqrt(prob[measurement])) 
+    namestack.pop()                             # pop stacks
+    return measurement
+    
 
-def TOFF3_gate(q1,q2,q3,q4):
-    pushQubit("temp",[1,0])
-    applyGate(TOFF_gate,q1,q2,"temp")
-    applyGate(TOFF_gate,"temp",q3,q4)
-    applyGate(TOFF_gate,q1,q2,"temp")
-    measureQubit("temp")
 
-def TOFFn_gate(ctl,result):
-    n = len(ctl)
-    if n == 0:
-        applyGate(X_gate,result)
-    if n == 1:
-        applyGate(CNOT_gate,ctl[0],result)
-    elif n == 2:
-        applyGate(TOFF_gate,ctl[0],ctl[1],result)
-    elif n > 2:
-        k=0
-        while "temp"+str(k) in namestack:
-            k=k+1
-        temp = "temp"+str(k)
-        pushQubit(temp,[1,0])
-        applyGate(TOFF_gate,ctl[0],ctl[1],temp)
-        ctl.append(temp)
-        TOFFn_gate(ctl[2:],result)
-        applyGate(TOFF_gate,ctl[0],ctl[1],temp)
-        measureQubit(temp)
+X_gate = np.array([[0, 1],[1, 0]])
+H_gate = np.array([[1, 1],[1,-1]])*np.sqrt(1/2)
+Z_gate = H_gate @ X_gate @ H_gate
+
+def sample_phaseOracle(qubits):
+    applyGate(X_gate,qubits[1])
+    applyGate(Z_gate,*namestack)
+    applyGate(X_gate,qubits[1])
 
 def zero_phaseOracle(qubits):
     for qubit in qubits:
@@ -82,11 +83,6 @@ def zero_phaseOracle(qubits):
     applyGate(Z_gate,*namestack)
     for qubit in qubits:
         applyGate(X_gate,qubit)
-
-def sample_phaseOracle(qubits):
-    applyGate(X_gate,qubits[1])
-    applyGate(Z_gate,*namestack)
-    applyGate(X_gate,qubits[1])
 
 def groverSearch(n, printProb=True):
     optimalTurns = int(np.pi/4*np.sqrt(2**n)-1/2)
@@ -106,43 +102,16 @@ def groverSearch(n, printProb=True):
         print(measureQubit(qubit),end="")
 
 
-X_gate = np.array([[0, 1],
-                   [1, 0]])
-
-Z_gate = np.array([[1, 0],
-                   [0,-1]])
-
-H_gate = np.array([[1, 1],
-                   [1,-1]]) * np.sqrt(1/2)
-
-T_gate = np.array([[1,                0],
-                   [0,np.exp(np.pi/-4j)]])
-
-Tinv_gate = np.array([[1, 0],
-                      [0,np.exp(np.pi/4j)]])
-
-CNOT_gate = np.array([[1, 0, 0, 0],
-                      [0, 1, 0, 0],
-                      [0, 0, 0, 1],
-                      [0, 0, 1, 0]])
-
-SWAP_gate = np.array([[1, 0, 0, 0],
-                      [0, 0, 1, 0],
-                      [0, 1, 0, 0],
-                      [0, 0, 0, 1]])
-
-TOFF_gate = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 1, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 1, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 1, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 1, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 1, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 1],
-                      [0, 0, 0, 0, 0, 0, 1, 0]])
-
-# tests
+if pt.cuda.is_available():
+    workspace = pt.tensor([[1.]],device=pt.device('cuda'),
+                                 dtype=pt.float32)
+    t = time.process_time()
+    groverSearch(16, printProb=False)
+    print(time.process_time() - t)
 
 
-workspace = np.array([[1.]])
-groverSearch(6)
-print()
+workspace = pt.tensor([[1.]],device=pt.device('cpu'),
+                             dtype=pt.float32)
+t = time.process_time()
+groverSearch(16, printProb=False)
+print( time.process_time() - t)
