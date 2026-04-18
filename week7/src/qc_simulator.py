197f9b0153415e384ea1f484e5045950d92b8ee6
Albert <albertmilicip23@gmail.com>
2026-04-18 13:31:59 +0100

    improved simulator with named qubits

--- week7/src/qc_simulator.py
+++ week7/src/qc_simulator.py
@@ -1,50 +1,58 @@
 import numpy as np
 
 workspace = np.array([[1.]])
+namestack = []
 
-def pushQubit(weights):
+def pushQubit(name,weights):
     global workspace
+    global namestack
+    if workspace.shape == (1,1):
+        namestack = []
+    namestack.append(name)
+    weights = weights/np.linalg.norm(weights)
+    weights = np.array(weights,dtype=workspace[0,0].dtype)
     workspace = np.reshape(workspace,(1,-1))
     workspace = np.kron(workspace,weights)
 
-def applyGate(gate):
+def tosQubit(name):
     global workspace
-    workspace = np.reshape(workspace,(-1,gate.shape[0]))
-    np.matmul(workspace,gate.T,out=workspace)
-
-def tosQubit(k):
-    global workspace
+    global namestack
+    k = len(namestack)-namestack.index(name)
     if k > 1:
+        namestack.append(namestack.pop(-k))
         workspace = np.reshape(workspace,(-1,2,2**(k-1)))
         workspace = np.swapaxes(workspace,-2,-1)
 
-def probQubit():
+def applyGate(gate,*names):
     global workspace
-    workspace = np.reshape(workspace,(-1,2))
-    return np.linalg.norm(workspace,axis=0)**2
+    for name in names:
+        tosQubit(name)
+    workspace = np.reshape(workspace,(-1,gate.shape[0]))
+    np.matmul(workspace,gate.T,out=workspace)
 
-def measureQubit():
+def probQubit(name):
     global workspace
-    prob = probQubit()
+    tosQubit(name)
+    workspace = np.reshape(workspace,(-1,2))
+    prob = np.linalg.norm(workspace,axis=0)**2
+    return prob/prob.sum()
+
+def measureQubit(name):
+    global workspace
+    global namestack
+    prob = probQubit(name)
     measurement = np.random.choice(2,p=prob)
     workspace = (workspace[:,[measurement]]/
-    np.sqrt(prob[measurement]))
+                 np.sqrt(prob[measurement]))
+    namestack.pop()
     return str(measurement)
+
 
 X_gate = np.array([[0, 1],
                    [1, 0]])
 
-Y_gate = np.array([[ 0,-1j],
-                   [1j,  0]])
-
-Z_gate = np.array([[1, 0],
-                   [0,-1]])
-
 H_gate = np.array([[1, 1],
                    [1,-1]]) * np.sqrt(1/2)
-
-S_gate = np.array([[1, 0],
-                   [0,1j]])
 
 T_gate = np.array([[1,                0],
                    [0,np.exp(np.pi/-4j)]])
@@ -52,31 +60,10 @@ T_gate = np.array([[1,                0],
 Tinv_gate = np.array([[1, 0],
                       [0,np.exp(np.pi/4j)]])
 
-def P_gate(phi):
-    return np.array([[1,             0],
-                     [0,np.exp(phi*1j)]])
-
-def Rx_gate(theta):
-    return np.array([[np.cos(theta/2),-1j*np.sin(theta/2)],
-                     [-1j*np.sin(theta/2),np.cos(theta/2)]])
-
-def Ry_gate(theta):
-    return np.array([[np.cos(theta/2),-np.sin(theta/2)],
-                     [np.sin(theta/2), np.cos(theta/2)]])
-
-def Rz_gate(theta):
-    return np.array([[np.exp(-1j*theta/2),                0],
-                     [                  0,np.exp(1j*theta/2)]])
-
 CNOT_gate = np.array([[1, 0, 0, 0],
                       [0, 1, 0, 0],
                       [0, 0, 0, 1],
                       [0, 0, 1, 0]])
-
-CZ_gate = np.array([[1, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0,-1]])
 
 SWAP_gate = np.array([[1, 0, 0, 0],
                       [0, 0, 1, 0],
@@ -94,48 +81,60 @@ TOFF_gate = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
 
 # tests
 
-workspace = np.array([[1.]])
-pushQubit([1,0])
-print("input", workspace)
-applyGate(X_gate)
-print("NOT output", workspace)
 
 workspace = np.array([[1.]])
-pushQubit([1,0])
-print("input", workspace)
-applyGate(H_gate)
-print("H output", workspace)
+pushQubit("Q1",[1,1])
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+pushQubit("Q2",[0,1])
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+tosQubit("Q1")
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+applyGate(H_gate,"Q2")
+print(np.reshape(workspace,(1,-1)))
+print(namestack)
+
 
 workspace = np.array([[1.]])
-pushQubit([1,0])
-pushQubit([0.6,0.8])
-print("before swap", workspace)
-applyGate(SWAP_gate)
-print("after swap", workspace)
+pushQubit("Q1",[1,0])
+applyGate(H_gate,"Q1")
+print("Q1 probabilities:", probQubit("Q1"))
+pushQubit("Q2",[0.6,0.8])
+print("Q2 probabilities:", probQubit("Q2"))
+print(measureQubit("Q1"), measureQubit("Q2"))
 
-workspace = np.array([[1.]])
-pushQubit([1,0])
-pushQubit([0.6,0.8])
-print("before tos", workspace)
-tosQubit(2)
-print("after tos", np.reshape(workspace,(1,-1)))
 
-workspace = np.array([[1.]])
-for n in range(30):
-    pushQubit([0.6,0.8])
-    print(measureQubit(), end="")
-print()
+def toffEquiv_gate(q1,q2,q3):
+    applyGate(H_gate,q3)
+    applyGate(CNOT_gate,q2,q3)
+    applyGate(Tinv_gate,q3)
+    applyGate(CNOT_gate,q1,q3)
+    applyGate(T_gate,q3)
+    applyGate(CNOT_gate,q2,q3)
+    applyGate(Tinv_gate,q3)
+    applyGate(CNOT_gate,q1,q3)
+    applyGate(T_gate,q2)
+    applyGate(T_gate,q3)
+    applyGate(H_gate,q3)
+    applyGate(CNOT_gate,q1,q2)
+    applyGate(T_gate,q1)
+    applyGate(Tinv_gate,q2)
+    applyGate(CNOT_gate,q1,q2)
 
-workspace = np.array([[1.]])
+workspace = np.array([[1.+0j]])
 for i in range(16):
-    pushQubit([1,0])
-    applyGate(H_gate)
-    pushQubit([1,0])
-    applyGate(H_gate)
-    pushQubit([1,0])
-    applyGate(TOFF_gate)
-    q3 = measureQubit()
-    q2 = measureQubit()
-    q1 = measureQubit()
-    print(q1+q2+q3,end=",")
+    pushQubit("Q1",[1,1])
+    pushQubit("Q2",[1,1])
+    pushQubit("Q3",[1,0])
+    toffEquiv_gate("Q1","Q2","Q3")
+    print(measureQubit("Q1")+measureQubit("Q2")+
+          measureQubit("Q3"), end=",")
 print()
